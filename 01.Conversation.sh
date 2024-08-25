076b7d688fc3474761e86f5440b874b8615394e9
suresh <saise7028@gmail.com>
2024-08-25 05:59:59 +0530

    coversation between two persons

---
+++ 01.Conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#conversation between two persons
+echo "Ramesh:: Hi Suresh How are you"
+echo "suresh:: Hello ramesh I am fine how are you doing?"
+echo "Ramesh:: I am doing good suresh,what's going on?"
+echo "suresh:: I have started for learning java script course"
+echo "Ramesh:: where did you joined course, fee's and course duration"
+echo "suresh:: it was online course you can join online/off line"
