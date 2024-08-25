b683fb52c1585a1c6a8c34b2aaacfe6e430297e1
suresh <saise7028@gmail.com>
2024-08-25 07:35:02 +0530

    changes coversation names

---
+++ 03.variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PERSON1=philip
+PERSON2=Shareesh
+
+echo "$PERSON1:: Hi $PERSON2 How are you"
+echo "$PERSON2:: Hello $PERSON1 I am fine how are you doing?"
+echo "$PERSON1:: I am doing good $PERSON2,what's going on?"
+echo "$PERSON2:: I have started for learning java script course"
+echo "$PERSON1:: where did you joined course, fee's and course duration"
+echo "$PERSON2:: it was online course you can join online/off line"
