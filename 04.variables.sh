16f534ab6ad7c111f4f8a36b764f1778c7b9e503
suresh <saise7028@gmail.com>
2024-08-26 10:59:02 +0530

    variables

---
+++ 04.variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "please enter your username:: "
+read  username
+echo "entered your username::$username"
+echo "please enterd your password:: "
+read  password
+echo "entered your password::$password"
