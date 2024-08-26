d6968122a709f1735e97f5c35e2e2428b7e734b3
suresh <saise7028@gmail.com>
2024-08-26 11:27:22 +0530

    hideing user info

---
+++ 05.variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "please enter your username:: "
+read  -s username # -s --> is hide the information,read --> it can read the information from variables
+echo "entered your username::$username"
+echo "please enterd your password:: "
+read  -s password
+echo "entered your password::$password"
