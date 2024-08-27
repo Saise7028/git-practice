6ceb23d6b7353e15d17bb5c6cae3761a1786689e
suresh <saise7028@gmail.com>
2024-08-27 19:45:45 +0530

    functions

--- 10.functions.sh
+++ 10.functions.sh
@@ -19,23 +19,23 @@ VALIDATE(){
         echo "$2 is...success"    
 }
 
-# dnf list installed git
-# if [ $? -ne 0 ]
-# echo "if git is not installed.going to install it"
-# dnf install git -y
-# VALIDATE $? installing git
-# else 
-#     echo "git is already installed,nothing to do.."
-# fi
+dnf list installed git
+if [ $? -ne 0 ]
+echo "if git is not installed.going to install it"
+dnf install git -y
+VALIDATE $? installing git
+else 
+    echo "git is already installed,nothing to do.."
+fi
 
-# dnf list installed mysql
-# if [ $? -ne 0 ]
-# echo "mysql not installed.going to install it"
-# dnf install mysql -y
-# VALIDATE $? installing mysql
-# else
-#     echo "my sql already installed,nothing to do.."
-# fi
+dnf list installed mysql
+if [ $? -ne 0 ]
+echo "mysql not installed.going to install it"
+dnf install mysql -y
+VALIDATE $? installing mysql
+else
+    echo "my sql already installed,nothing to do.."
+fi
 
 
 
