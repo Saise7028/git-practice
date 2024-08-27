b35f24604c2a7068adda29683bf1fbb76b204593
suresh <saise7028@gmail.com>
2024-08-27 20:02:07 +0530

    functions

--- 10.functions.sh
+++ 10.functions.sh
@@ -25,18 +25,18 @@ dnf list installed git
 if [ $? -ne 0 ]
 echo "if git is not installed.going to install it"
 dnf install git -y
-VALIDATE $? installing git
+VALIDATE $? "installing git"
 else 
-    echo "git is already installed,nothing to do.."
+    echo "git is already installed.nothing to do.."
 fi
 
 dnf list installed mysql
 if [ $? -ne 0 ]
 echo "mysql not installed.going to install it"
 dnf install mysql -y
-VALIDATE $? installing mysql
+VALIDATE $? "installing mysql"
 else
-    echo "my sql already installed,nothing to do.."
+    echo "my sql already installed.nothing to do.."
 fi
 
 
