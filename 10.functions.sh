ba8f9870e414acf588ce5d32a7e347fb0293f368
suresh <saise7028@gmail.com>
2024-08-27 19:55:37 +0530

    functions

--- 10.functions.sh
+++ 10.functions.sh
@@ -16,28 +16,28 @@ VALIDATE(){
         echo "$2 is...failure"
         exit 1
     else 
-        echo "$2 is...success"    
+        echo "$2 is...success"
+    fi        
 }
 
-CHECK_ROOT
 
-dnf list installed git
-if [ $? -ne 0 ]
-echo "if git is not installed.going to install it"
-dnf install git -y
-VALIDATE $? installing git
-else 
-    echo "git is already installed,nothing to do.."
-fi
+# dnf list installed git
+# if [ $? -ne 0 ]
+# echo "if git is not installed.going to install it"
+# dnf install git -y
+# VALIDATE $? installing git
+# else 
+#     echo "git is already installed,nothing to do.."
+# fi
 
-dnf list installed mysql
-if [ $? -ne 0 ]
-echo "mysql not installed.going to install it"
-dnf install mysql -y
-VALIDATE $? installing mysql
-else
-    echo "my sql already installed,nothing to do.."
-fi
+# dnf list installed mysql
+# if [ $? -ne 0 ]
+# echo "mysql not installed.going to install it"
+# dnf install mysql -y
+# VALIDATE $? installing mysql
+# else
+#     echo "my sql already installed,nothing to do.."
+# fi
 
 
 
