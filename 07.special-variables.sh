ed0be3807454e7b59796a87d2107431696e2cda9
suresh <saise7028@gmail.com>
2024-08-27 09:57:12 +0530

    speccial-variables

--- 07.special-variables.sh
+++ 07.special-variables.sh
@@ -3,6 +3,6 @@
 echo "all the variables passed to the script: $@"
 echo "arguments passed through the script: $#"
 echo "To know the script name: $0"
-echo "Present working directory: $pwd"
-echo "user home directory: $home"
+echo "Present working directory: $PWD"
+echo "user home directory: $HOME"
 echo "PID of script executing now: $$" 
