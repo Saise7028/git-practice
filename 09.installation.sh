e4b651df4d47d70ce210a9d10f51e91479d01ec8
suresh <saise7028@gmail.com>
2024-08-27 14:45:29 +0530

    installation

--- 09.installation.sh
+++ 09.installation.sh
@@ -1,12 +1,26 @@
 #!/bin/bash 
 
-USERID=$(id -u)
-# echo "user id is: $USERID"
+USERID=$(ID -u)
+#echo "user ID is $USERID"
 
 if [ $USERID -ne 0 ]
-then
-    echo "installing the script with root preveliges"
+then 
+    echo "installing the script with root previliages"
     exit 1
 fi
-
+dnf list installed git # checking wether the git installed are not.
+if [  $? -ne 0 ]
+then
+    echo "git is not installed.going to install it.."
 dnf install git -y
+if [ $? -ne 0 ]
+then 
+    echo "git is intalled are not check it.."
+    exit 1
+else
+    echo "git installation is success.."
+fi
+else 
+    echo "git is already installed nothing to do.."
+fi 
+               
