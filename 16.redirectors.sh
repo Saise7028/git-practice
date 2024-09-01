b4dfda1939d0aa6b36601016cb47252856c94094
suresh <saise7028@gmail.com>
2024-09-01 13:39:10 +0530

    logs

--- 16.redirectors.sh
+++ 16.redirectors.sh
@@ -10,6 +10,7 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
 CHECK_ROOT(){
        if [ $USERID -ne 0 ]
@@ -28,3 +29,27 @@ VALIDATE(){
              echo -e "$2 is...$G Success $N" &>>LOG_FILE
         fi         
 }
+
+USAGE(){
+    if [ $# -ne 0 ]
+      then 
+          echo -e "$R USAGE:: $N sudo 16.redirectors.sh package1 package2..."
+          exit 1
+    fi      
+}
+
+CHECK_ROOT
+
+for package in $@
+           do 
+           dnf installed $package &>>$LOG_FILE
+              if [ $? -ne 0 ]
+                then 
+                    echo "$package is not installed.then install it.." &>>$LOG_FILE
+            dnf install $package -y &>>LOG_FILE
+            VALIDATE $? "installing $package" &>>LOG_FILE
+              else 
+                  echo -e "$package is already $Y installed.nothing to do.. $N" &>>LOG_FILE
+                  exit 1
+               fi   
+            done                
