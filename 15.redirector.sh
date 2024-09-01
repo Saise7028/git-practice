2ed45301693a86a961e3816ad707517420832065
suresh <saise7028@gmail.com>
2024-09-01 08:09:28 +0530

    redirector

--- 15.redirector.sh
+++ 15.redirector.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# /var/log/shell-script/16.redirectors.sh<timestamp>.log
 
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
@@ -8,12 +7,12 @@ TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
-USERID=$(id -u)
 
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
