9bed09a444347a671def41167012c6dd80ba28b2
suresh <saise7028@gmail.com>
2024-09-01 12:49:03 +0530

    logs

---
+++ 16.redirectors.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT(){
+       if [ $USERID -ne 0 ]
+         then 
+             echo -e "$R please run the script with root preveleges $N" &>>LOG_FILE
+             exit 1
+        fi     
+}
+
+VALIDATE(){
+       if [ $1 -ne 0 ]
+         then 
+             echo "$2 is...$R Failure $N" &>>LOG_FILE
+             exit 1
+         else
+             echo "$2 is...$G Success $N" &>>LOG_FILE
+        fi         
+}
