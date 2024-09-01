fcf566443d64999c48e953c53fb042c5f0e8af20
suresh <saise7028@gmail.com>
2024-09-01 07:05:06 +0530

    redirector

--- 15.redirector.sh
+++ 15.redirector.sh
@@ -3,7 +3,7 @@
 # /var/log/shell-script/16.redirectors.sh<timestamp>.log
 
 LOGS_FOLDER="/var/log/shell_script"
-SCRIPT_NAME=$(echo $0 | cut -d "." F1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE=$"LOGS_FOLDER/SCRIPT_NAME/TIMESTAMP.log"
 
