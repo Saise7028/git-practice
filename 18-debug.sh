90a5e174a1078855471885d4cdd9ba758c389060
suresh <saise7028@gmail.com>
2024-09-03 10:45:50 +0530

    debug

---
+++ 18-debug.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+
+echo "hello world program execution"
+echoooo "progarm is not executed"
+echo "hello world program is success"
+echoooo "promotion code not installed"
+echo "program code was error"
