28246cd48d69ab0d9d40380a28036a66d8f6f717
suresh <saise7028@gmail.com>
2024-09-03 13:20:50 +0530

    delete

---
+++ 19-Delete oldlogs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Delecting old log files
+
+SOURECE_DIR="/home/ec2-user/files"
+
+if [ -d $SOURECE_DIR ]
+  then 
+      echo "$SOURECE_DIR is Exists"
+  else
+      echo "$SOURCE_DIR is not Exists"
+      exit 1
+fi
+
+# find -path -name <filename> -mtime n --> mtime--reperesent the meantime here '-' old time and '+'present time
+
+FILES=$(find $(SOURECE_DIR) -name "*.log" -mtime +10)
+
+echo "Files: $Files"         
